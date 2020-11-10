4229a3a65f4d8eb16deecddeec47e3a38a78aa77
muNeves3 <murilogrilosn@gmail.com>
2020-11-10 17:51:54 -0300

    challange finished

--- src/__tests__/App.tsx
+++ src/__tests__/App.tsx
@@ -22,7 +22,7 @@ jest.mock('../utils/formatValue.ts', () => ({
 }));
 
 import React from 'react';
-import { render, fireEvent, act } from '@testing-library/react';
+import { render, fireEvent, act, getByText } from '@testing-library/react';
 import MockAdapter from 'axios-mock-adapter';
 import api from '../services/api';
 import App from '../App';
@@ -30,7 +30,7 @@ import App from '../App';
 const apiMock = new MockAdapter(api);
 
 const wait = (amount = 0): Promise<void> => {
-  return new Promise((resolve) => setTimeout(resolve, amount));
+  return new Promise(resolve => setTimeout(resolve, amount));
 };
 
 const actWait = async (amount = 0): Promise<void> => {
